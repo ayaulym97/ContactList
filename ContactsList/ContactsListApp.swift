1db0892ba7d4aeb0745b40df66ddfe30b54c6ef6
AAAlzhanova <AAAlzhanova@beeline.kz>
2021-09-17 17:56:30 +0600

    Initial Commit

---
+++ ContactsList/ContactsListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsListApp.swift
+//  ContactsList
+//
+//  Created by Ayaulym Alzhanova on 17.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct ContactsListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
